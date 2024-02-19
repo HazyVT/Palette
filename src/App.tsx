6a3a6c62478bcc935b23731f8cb46b49c072c0e9
Mohammed Salim <renablehots@gmail.com>
2024-02-19 14:30:44 +0400

    Created Color Scheming Project
    - Initialized Project
    - Created dashboard component
    - Created color linking
    - Removed unnecessary files

--- src/App.tsx
+++ src/App.tsx
@@ -1,147 +1,27 @@
-import { useState, useEffect, useRef, MutableRefObject, FocusEvent } from 'react';
-import { supabase } from './database';
-import { returnType } from './exports';
-import { Box, Spinner, TableContainer, Table, Tr, Th, Thead, Tbody, Input, Image } from '@chakra-ui/react';
-import { data } from './fs.json';
+import { useState } from "react"
+import Dashboard from "./components/Dashboard"
+import { Box, Button } from "@chakra-ui/react";
 
 function App() {
 
-  const [ loading, setLoading ] = useState(true);
-  const [ paramsl ] = useState<Map<number, {type: string, value: string}>>(new Map());
-
-  const tl = useRef() as MutableRefObject<HTMLImageElement>;
-  const tc = useRef() as MutableRefObject<HTMLImageElement>;
-  const tr = useRef() as MutableRefObject<HTMLImageElement>;
-
-  const ml = useRef() as MutableRefObject<HTMLImageElement>;
-  const mc = useRef() as MutableRefObject<HTMLImageElement>;
-  const mr = useRef() as MutableRefObject<HTMLImageElement>;
-
-  const bl = useRef() as MutableRefObject<HTMLImageElement>;
-  const bc = useRef() as MutableRefObject<HTMLImageElement>;
-  const br = useRef() as MutableRefObject<HTMLImageElement>;
-
-  const signIn = async () => {
-    await supabase.auth.signInWithPassword({
-      email: 'tempuser@gmail.com',
-      password: '12345678'
-    }).then(() => {
-      setLoading(false);
-    });
-  }
-
-  const findCard = async (e: FocusEvent<HTMLInputElement, Element>) => {
-    if (e.target.value != '') {
-      const url = "https://db.ygoprodeck.com/api/v7/cardinfo.php?name="+e.target.value;
-      const response = await fetch(url, {method: "GET"});
-      response.json().then((val: returnType) => {
-        val.data.forEach((card) => {
-          console.log(card);
-        })
-      })
-    }
-    
-  }
-
-  useEffect(() => {
-    if (localStorage.getItem("sb-svqubdprnzkhvgwslxzw-auth-token") == null) {
-      signIn();
-    } else {
-      setLoading(false);
-    }
-
-    const now = new Date();
-    const start = new Date(now.getFullYear(), 0, 0);
-    // eslint-disable-next-line @typescript-eslint/ban-ts-comment
-    //@ts-expect-error
-    const diff = now - start;
-    const oneDay = 1000 * 60 * 60 * 24;
-    const day = Math.floor(diff / oneDay);
-    
-    const todaysData = data[day];
-
-    todaysData.forEach((val, index) => {
-      paramsl.set(index, val);
-    })
-    
-  }, [])
-
-  function AppUI() {
-    return (
-      <Box display='flex' flexDir='column' w='100vw' h='100vh' justifyContent='center' alignItems='center'>
-        <TableContainer marginTop={16}>
-          <Table>
-            <Thead>
-              <Tr>
-                <Th></Th>
-                <Th>{paramsl.get(0)?.type + ' ' + paramsl.get(0)?.value}</Th>
-                <Th>{paramsl.get(1)?.type + ' ' + paramsl.get(1)?.value}</Th>
-                <Th>{paramsl.get(2)?.type + ' ' + paramsl.get(2)?.value}</Th>
-              </Tr>
-            </Thead>
-            <Tbody>
-              <Tr>
-                <Th>{paramsl.get(3)?.type + ' ' + paramsl.get(3)?.value}</Th>
-                <Th w='40' h='40'>
-                  <Input placeholder='' onBlur={(e) => findCard(e)}/>
-                  <Image ref={tl}/>
-                </Th>
-                <Th w='40' h='40'>
-                  <Input placeholder=''/>
-                  <Image ref={tc} />
-                </Th>
-                <Th w='40' h='40'>
-                  <Input placeholder=''/>
-                  <Image ref={tr} />
-                </Th>
-              </Tr>
-              <Tr>
-                <Th>{paramsl.get(4)?.type + ' ' + paramsl.get(4)?.value}</Th>
-                <Th w='40' h='40'>
-                  <Input placeholder=''/>
-                  <Image ref={ml} />
-                </Th>
-                <Th w='40' h='40'>
-                  <Input placeholder=''/>
-                  <Image ref={mc} />
-                </Th>
-                <Th w='40' h='40'>
-                  <Input placeholder=''/>
-                  <Image ref={mr} />
-                </Th>
-              </Tr>
-              <Tr>
-                <Th>{paramsl.get(5)?.type + ' ' + paramsl.get(5)?.value}</Th>
-                <Th w='40' h='40'>
-                  <Input placeholder=''/>
-                  <Image ref={bl} />
-                </Th>
-                <Th w='40' h='40'>
-                  <Input placeholder=''/>
-                  <Image ref={bc} />
-                </Th>
-                <Th w='40' h='40'>
-                  <Input placeholder=''/>
-                  <Image ref={br} />
-                </Th>
-              </Tr>
-            </Tbody>
-          </Table>
-        </TableContainer>
-      </Box>
-    )
-  }
+  const [ bgColor, setBgColor ] = useState('#f6f1f9');
+  const [ primaryColor, setPrimaryColor ] = useState('#8a4cb5');
+  const [ secondaryColor, setSecondaryColor ]  = useState('#d496cc');
+  const [ accentColor, setAccentColor ] = useState('#c87baa');
 
   return (
     <>
-      <Box>
-        {loading ? 
-          <Box w='100vw' h='100vh' display='flex' justifyContent={'center'} alignItems='center'>
-            <Spinner w={24} h={24} color='red.300' thickness='4px' emptyColor='gray.200'/>
-          </Box>
-        :
-          <AppUI />
-        }
+      <Dashboard background={bgColor} primary={primaryColor} secondary={secondaryColor} accent={accentColor}/>
+      <Box pos='fixed' w='10vw' h='100vh' boxShadow='md' left='0' top='0' display='flex' flexDir='column' alignItems='center' padding={12} justifyContent='space-between'>
+        <Button colorScheme="teal">Generate</Button>
+        <Box>
+          <Button w='8vw' bgColor={bgColor} _hover={{bgColor: bgColor}}>Background</Button>
+          <Button w='8vw' marginTop={4} bgColor={primaryColor} _hover={{bgColor: primaryColor}}>Primary</Button>
+          <Button w='8vw' marginTop={4} bgColor={secondaryColor} _hover={{bgColor: secondaryColor}}>Secondary</Button>
+          <Button w='8vw' marginTop={4} bgColor={accentColor} _hover={{bgColor: accentColor}}>Accent</Button>
+        </Box>
+        <Button w='8vw' colorScheme="teal">Export</Button>
+
       </Box>
     </>
   )
---
+++ src/components/Dashboard.tsx
@@ -0,0 +1,117 @@
+import { Avatar, AvatarBadge, Box, Heading, Icon, Image, Input, InputGroup, InputLeftElement, Text } from "@chakra-ui/react";
+import { WiStars } from "react-icons/wi";
+import { FaHome, FaUsers, FaSearch, FaHeart } from "react-icons/fa";
+import { RiQuestionnaireFill } from "react-icons/ri";
+import { AiFillMessage } from "react-icons/ai";
+import { IoNotifications } from "react-icons/io5";
+import { FaGear, FaMessage } from "react-icons/fa6";
+
+
+
+import { useState } from "react";
+
+
+
+export default function Dashboard(props: {background: string, primary: string, secondary: string, accent: string}) {
+
+    const [ active, setActive ] = useState('home');
+    const accent = props.accent;
+    const primary = props.primary;
+    const secondary = props.secondary;
+
+    function SidebarTab(props: {icon: typeof FaHome, text: string}) {
+        return (
+            <Box 
+                display='flex' 
+                alignItems='center' 
+                bgColor={active === props.text.toLocaleLowerCase() ? accent : 'none'} 
+                padding={2} borderRadius={'4px'} 
+                color={active === props.text.toLowerCase() ? 'white' : 'black'}
+                onClick={() => setActive(props.text.toLowerCase())}
+                cursor={"pointer"}
+            >
+                <Icon as={props.icon} />
+                <Text fontSize={16} fontWeight={500} marginLeft={2}>{props.text}</Text>
+            </Box>
+        )
+    }
+
+    function SidePanel() {
+        return (
+            <Box w='12vw' h='60vh' bgColor={props.background}>
+                <Box marginTop={4} marginLeft={2}>
+                    <SidebarTab icon={FaHome} text="Home" />
+                    <SidebarTab icon={RiQuestionnaireFill} text="Questions" />
+                    <SidebarTab icon={AiFillMessage} text="Messages" />
+                    <SidebarTab icon={FaUsers} text="Friends" />
+                </Box>
+            </Box>
+        )
+        
+    }
+
+    function Navigation() {
+        return (
+            <Box display='flex' justifyContent='space-between' h='fit-content'>
+                    <Box display='flex' alignItems='center'>
+                        <Icon as={WiStars} w={10} h={10}/>
+                        <Text fontWeight={600} fontSize={24}>QUESTIONS</Text>
+                    </Box>
+                    <InputGroup w='20vw'>
+                        <InputLeftElement>
+                            <Icon as={FaSearch} />
+                        </InputLeftElement>
+                        <Input variant='flushed' focusBorderColor={props.accent}/>
+                    </InputGroup>
+                    <Box display='flex' alignItems='center'>
+                        <Icon _hover={{color: secondary}} as={FaGear} w={5} h={5} cursor="pointer" />
+                        <Icon _hover={{color: secondary}} as={IoNotifications} w={6} h={6} marginLeft={4} cursor="pointer" />
+                        <Avatar src="https://avatar.iran.liara.run/public" marginLeft={4} cursor={"pointer"}>
+                            <AvatarBadge boxSize='1em' bgColor='green.300'/>
+                        </Avatar>
+                    </Box>
+                </Box>
+        )
+    }
+
+    function QuestionBox(props: {name: string, text: string, image: string, icon: string}) {
+        return (
+            <Box w='40vw' h="fit-content" bgColor={secondary} padding={4} borderRadius={'10px'} marginTop={4}>
+                <Box display='flex' alignItems='center'>
+                    <Avatar src={props.icon}/>
+                    <Text marginLeft={4} fontSize={18} fontWeight={500}>{props.name}</Text>
+                </Box>
+                <Text fontSize={18} marginTop={4}>{props.text}</Text>
+                <Image src={props.image} borderRadius={'10px'} marginTop={2} w='800px'/>
+                <Box marginTop={2} display='flex'>
+                    <Box display='flex' alignItems='center'>
+                        <Icon as={FaMessage} w={'1rem'} h={'1rem'}/>
+                        <Text marginLeft={2}>24</Text>
+                    </Box>
+                    <Box display='flex' alignItems='center' marginLeft={4}>
+                        <Icon as={FaHeart} w={'1.1rem'} h={'1.1rem'}/>
+                        <Text marginLeft={1.2}>31</Text>
+                    </Box>
+                </Box>
+            </Box>
+        )
+
+    }
+    
+    return (
+        <Box w='100wh' h='100vh' display='flex' flexDir='column' alignItems='center' justifyContent='center' marginLeft={44}>
+            <Box w='80vw' h='80vh' bgColor={props.background} borderRadius={'10px'} padding={4}>
+                <Navigation />
+                <Box display='flex'>
+                    <SidePanel />
+                    <Box w='68vw' h='68vh' marginTop={4} marginLeft={4} paddingLeft={12} overflow={'scroll'}>
+                        <Text fontSize={34} fontWeight={600}>ALL QUESTIONS</Text>
+                        <QuestionBox icon={"https://avatar.iran.liara.run/public/26"} name={"Andrew Botez"} text={"How do I create a variable that updates in react? Heres what I tried."} image={"/codexample.jpeg"} />
+                        <QuestionBox icon={"https://avatar.iran.liara.run/public/94"} name={"Priscilla Dunn"} text={"How do I create a variable that updates in react? Heres what I tried."} image={"/codexample.jpeg"} />
+                        <QuestionBox icon={"https://avatar.iran.liara.run/public/74"} name={"Jessica Gardner"} text={"How do I create a variable that updates in react? Heres what I tried."} image={"/codexample.jpeg"} />
+                    </Box>
+                </Box>
+            </Box>
+        </Box>
+    )
+}
